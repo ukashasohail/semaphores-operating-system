a2910612bb747be711c0e70b6a680ab2cd9b9420
ukashasohail <ukasha909@outlook.com>
2020-07-28 19:16:20 +0500

    final

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 // dummy code for semaphores
+// operating system by william stallings
 
 struct semaphore
 {
@@ -11,5 +12,13 @@ void semWait(semaphore s){
     if (s.count < 0){
         // place this process in queue
         // block this proc
+    }
+}
+
+void semSignal(semaphore s){
+    s.count++;
+    if (s.count <=0){
+        // remove a proc P from s.queue
+        // place proc P on ready list
     }
 }
